277a8207e1c9582d89b429cdc9919ce27ddc2e42
no0bAuntor <mbauntor150@gmail.com>
2024-10-26 02:45:51 +0600

    --

---
+++ 27_keywordArguement.py
@@ -0,0 +1,7 @@
+def hello(greeting, firstName, lastName):
+    print(f"{greeting} {firstName} {lastName}")
+
+
+hello("Hello", lastName="Billah", firstName="Mutasam")
+
+print("1", "2", "3", "4", sep="-")  # "sep" is an keyword argument
